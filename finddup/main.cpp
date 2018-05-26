29cf71bd14143a67a099e31666875507b3a849c8
dany <dany@danys-mac-pro.local>
2018-05-26 12:53:06 +0800

    Initial Commit

---
+++ finddup/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  finddup
+//
+//  Created by dany on 2018/5/26.
+//  Copyright © 2018年 dany. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
